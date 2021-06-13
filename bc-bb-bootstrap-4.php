c59794e44b55a963a6ebe063c8bb0f92e03c01f0
beavercoffee <beavercoffee@vidsoe.com>
2021-06-13 16:07:00 -0600

    BC

---
+++ bc-bb-bootstrap-4.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Author: Beaver Coffee
+Author URI: https://beaver.coffee
+Description: Add Bootstrap 4 to Beaver Builder's default styles.
+Domain Path:
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Network: true
+Plugin Name: BC BB Bootstrap 4
+Plugin URI: https://github.com/beavercoffee/bc-bb-bootstrap-4
+Requires at least: 5.7
+Requires PHP: 5.6
+Text Domain: bc-bb-bootstrap-4
+Version: 1.6.13
+*/
+
+if(defined('ABSPATH')){
+    require_once(plugin_dir_path(__FILE__) . 'classes/class-bc-bb-bootstrap-4.php');
+    BC_BB_Bootstrap_4::get_instance(__FILE__);
+}
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== BC BB Bootstrap 4 ===
+Contributors: beavercoffee
+Donate link: https://beaver.coffee
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Requires at least: 5.7
+Requires PHP: 5.6
+Stable tag: 1.6.13
+Tags:
+Tested up to: 5.7.2
+
+BC BB Bootstrap 4
+
+== Description ==
+
+Add Bootstrap 4 to Beaver Builder's default styles.
+
+== Changelog ==
+
+[Changelog »](https://github.com/beavercoffee/bc-bb-bootstrap-4 "BC BB Bootstrap 4")
